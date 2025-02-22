8e782469c6508ab260985b428f54fe6d5b726e89
muhammadaminusmonov <muhammadamincoder@gmail.com>
2025-02-22 11:34:07 +0500

    added Person class

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def greeting():
-    print('hello world')
 
-greeting()
+class Person:
+    def __init__(self, name, surname, age):
+        self.name = name
+        self.surname = surname
+        self.age = age
+
+    def __str__(self):
+        return f"I am {self.name} {self.surname} and i am {self.age} old"
