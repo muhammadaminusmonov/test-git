3e890adaf6ec25eaf4c3d20be3075e7c185581af
muhammadaminusmonov <muhammadamincoder@gmail.com>
2025-02-22 11:53:31 +0500

    added bio method

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ class Person:
         self.name = name
         self.surname = surname
         self.age = age
+        self.info = None
 
     def __str__(self):
         return f"I am {self.name} {self.surname} and i am {self.age} old"
+
+    def bio(self, info):
+        self.info = info
